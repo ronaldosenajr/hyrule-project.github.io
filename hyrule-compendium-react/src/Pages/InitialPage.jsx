e979fddc0d9d155fc3f850d94de2ae35e9ae92c2
ronaldojr <ronaldosenajr@gmail.com>
2022-03-02 16:53:10 -0400

    Chama a api quando clica no botao de filtro ou no botao lets search, se já tiver os dados da api, só faz o filtro

--- hyrule-compendium-react/src/Pages/InitialPage.jsx
+++ hyrule-compendium-react/src/Pages/InitialPage.jsx
@@ -1,28 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '../Components/Card';
 import zeldaTrailer from '../media/zelda-trailer.mov';
 import triforcePng from '../media/zelda-botw-logo.png';
 
 export default function InitialPage() {
   const [allData, setAllData] = useState({});
+  const [filterValue, setFilterValue] = useState('all');
+  const [dataToDisplay, setDataToDisplay] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [canShowCards, setCanShowCards] = useState(false);
 
   const getData = async () => {
     const url = 'https://botw-compendium.herokuapp.com/api/v2';
     const result = await fetch(url).then((data) => data.json());
 
     setAllData({ ...result.data,
+      creatures: [...result.data.creatures.non_food],
+      food: [...result.data.creatures.food],
       all: [...result.data.creatures.food, ...result.data.creatures.non_food,
         ...result.data.equipment, ...result.data.materials,
         ...result.data.monsters, ...result.data.treasure] });
+    setLoading(false);
   };
+
+  useEffect(() => {
+    if (!loading) {
+      setDataToDisplay([...allData[filterValue]]);
+    }
+  }, [loading, filterValue]);
 
   const handleClick = async () => {
     if (!allData.all) {
+      setFilterValue('all');
       await getData();
-      setLoading(false);
     }
-    setLoading(false);
+    setCanShowCards(true);
+  };
+
+  const setFilterDisplay = (name) => {
+    if (!allData.all) {
+      getData();
+    }
+    setFilterValue(name);
+    setCanShowCards(true);
   };
 
   return (
@@ -51,9 +71,63 @@ export default function InitialPage() {
           </button>
         </div>
       </div>
+      <section id="filter-buttons">
+        <h2>Filter</h2>
+        <div id="nav-buttons">
+          <a
+            href="#content"
+            className="nav-btn"
+            onClick={ () => setFilterDisplay('all') }
+          >
+            <div id="all-btn">All</div>
+          </a>
+          <a
+            href="#content"
+            className="nav-btn"
+            onClick={ () => setFilterDisplay('creatures') }
+          >
+            <div id="creatures-btn">Creatures</div>
+          </a>
+          <a
+            href="#content"
+            className="nav-btn"
+            onClick={ () => setFilterDisplay('equipment') }
+          >
+            <div id="items-btn">Equipment</div>
+          </a>
+          <a
+            href="#content"
+            className="nav-btn"
+            onClick={ () => setFilterDisplay('food') }
+          >
+            <div id="food-btn">Food</div>
+          </a>
+          <a
+            href="#content"
+            className="nav-btn"
+            onClick={ () => setFilterDisplay('materials') }
+          >
+            <div id="materials-btn">Materials</div>
+          </a>
+          <a
+            href="#content"
+            className="nav-btn"
+            onClick={ () => setFilterDisplay('monsters') }
+          >
+            <div id="monsters-btn">Monsters</div>
+          </a>
+          <a
+            href="#content"
+            className="nav-btn"
+            onClick={ () => setFilterDisplay('treasure') }
+          >
+            <div id="treasure-btn">Treasure</div>
+          </a>
+        </div>
+      </section>
       <section id="content">
         {
-          (!loading && (allData.all.map((data) => (
+          (canShowCards && (dataToDisplay.map((data) => (
             <Card content={ data } key={ data.name } />))))
         }
       </section>
